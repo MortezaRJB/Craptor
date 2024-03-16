06e067143243b4101bad9edb9753f5ffaa5f7a32
MortezaRJB <morteza.rajabi1379@gmail.com>
2024-03-16 16:25:46 +0330

    created class for Orderbook

--- orderbook.py
+++ orderbook.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from typing import List
+from typing import List, Dict
 from datetime import datetime
 
 
@@ -18,4 +18,14 @@ class Limit:
     self.price                  = price
     self.orders: List[Order]    = None
     self.totalVolume            = Decimal('0')
+
+
+class Orderbook:
+
+  def __init__(self) -> None:
+    self.asks: List['Limit']                  = []
+    self.bids: List['Limit']                  = []
+    self.askLimits: Dict[Decimal, 'Limit']    = {}
+    self.bidLimits: Dict[Decimal, 'Limit']    = {}
+
 
