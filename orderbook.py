322e36afec1c21aef74e0a29a977f1e124675c65
MortezaRJB <morteza.rajabi1379@gmail.com>
2024-03-16 16:22:37 +0330

    created class for Limit

--- orderbook.py
+++ orderbook.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from typing import List
 from datetime import datetime
 
 
@@ -7,6 +8,14 @@ class Order:
   def __init__(self, size: Decimal, is_Bid: bool) -> None:
     self.size             = size
     self.is_Bid           = is_Bid
-    self.limit            = None
+    self.limit: Limit     = None
     self.timestamp        = datetime.now
+
+
+class Limit:
+
+  def __init__(self, price: Decimal) -> None:
+    self.price                  = price
+    self.orders: List[Order]    = None
+    self.totalVolume            = Decimal('0')
 
